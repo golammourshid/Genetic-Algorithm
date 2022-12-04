56e67b0831a5f0dbfa8ea6817411ad0abc033bf7
golammourshid <golammourshid100@gmail.com>
2022-12-04 09:38:43 +0600

    added object array and some modification

--- Genetic_Algorithm.cpp
+++ Genetic_Algorithm.cpp
@@ -5,26 +5,27 @@
 
 #include<iostream>
 #include <ctime>
-#include <windows.h>
-#include <unistd.h>
-#include <chrono>
+//#include <unistd.h>
+#include <stdlib.h>
+//#include <chrono>
 
 using namespace std;
 
 
 
 
-//This is the structure class of the inventory system.
+//This is the structure class of the Genetic Algorithm.
 class allRobot
 {
 public:
-    int robotWithPosition [200][12][12];
-    int robotWithGenes [200][16][5];
-    int robotWithEnergy [200][1];
-    int robotWithTurns [200][1]= {0};
+    int robotWithPosition [12][12];
+    int robotWithGenes [16][5];
+    int robotWithEnergy;
+    int robotWithTurns;
+    double robotAvgFitness;
     int Gene[16][5];
-    int Grid [12][12] = {0};
-    int initX, initY, randDir, robotSeq;
+    int Grid [12][12];
+    int initX, initY, randDir;
     allRobot()
     {
         srand(time(NULL));
@@ -35,48 +36,83 @@ public:
     void placeRobot();
     void robotRandDirection();
     void runRobot();
+    void sortRoboticGene(allRobot obj[]);
+    void Reproduction(allRobot obj[]);
 };
 
 
 int main ()
 {
-    allRobot all_robot;
+    int totalRobot = 5, totalGeneration = 10;
+    allRobot all_robot[totalRobot], tempRobot, generationCountRobot[totalGeneration];
     cout<<"Starting Genetic Algorithm SImulation..............."<<endl<<endl;
-    int twoHundred = 200;
-    all_robot.robotSeq = 0;
-    for(; all_robot.robotSeq<1; all_robot.robotSeq++)
+    int indx, generation;
+    for(generation = 0 ; generation<totalGeneration; generation++)
     {
-        //creating an Date type object
-        allRobot all_robot;
-        all_robot.robotWithEnergy[all_robot.robotSeq][0] = 5;
-        all_robot.geneCreate();
-        all_robot.gridCreate();
-        all_robot.placeRobot();
-        all_robot.robotWithPosition[all_robot.robotSeq][all_robot.initX][all_robot.initY];
-        //all_robot.Grid, all_robot.robotWithGenes, all_robot.robotWithEnergy,all_robot.robotWithPosition
-        all_robot.runRobot();
-        for(int i=0; i<16; i++)
-        {
-            for (int j=0; j<5; j++)
-            {
-                cout<<all_robot.Gene[i][j] <<" ";
+        //all_robot.robotSeq = 0;
+        int cntTurn = 0;
 
+        for(indx = 0; indx<totalRobot; indx++)
+        {
+            //creating an allRobot type object
+            all_robot[indx].robotWithEnergy = 5;
+            //cout<<"Initial Energy: "<<all_robot.robotWithEnergy[all_robot.robotSeq][0]<<endl;
+            if(generation==0)
+            {
+                all_robot[indx].geneCreate();
+                all_robot[indx].gridCreate();
+            }
+
+            all_robot[indx].placeRobot();
+            all_robot[indx].robotWithPosition[all_robot[indx].initX][all_robot[indx].initY];
+            //all_robot.Grid, all_robot.robotWithGenes, all_robot.robotWithEnergy,all_robot.robotWithPosition
+            all_robot[indx].runRobot();
+            for(int i=0; i<16; i++)
+            {
+                for (int j=0; j<5; j++)
+                {
+                    cout<<all_robot[indx].Gene[i][j] <<" ";
+
+                }
+                cout<<endl;
+            }
+            cout<<endl;
+            cout<<endl;
+            cout<<endl;
+            for(int i=0; i<12; i++)
+            {
+                for (int j=0; j<12; j++)
+                {
+                    cout<<all_robot[indx].Grid[i][j] <<" ";
+
+                }
+                cout<<endl;
+            }
+            cntTurn+=all_robot[indx].robotWithTurns;
+            cout<<"Total Turns: "<<all_robot[indx].robotWithTurns<<endl;
+        }
+        tempRobot.sortRoboticGene(all_robot);
+        tempRobot.Reproduction(all_robot);
+        for(int k=0; k<totalRobot; k++)
+        {
+            for(int i=0; i<16; i++)
+            {
+                for (int j=0; j<5; j++)
+                {
+                    cout<<all_robot[k].Gene[i][j] <<" ";
+
+                }
+                cout<<endl;
             }
             cout<<endl;
         }
-        cout<<endl;
-        cout<<endl;
-        cout<<endl;
-        for(int i=0; i<12; i++)
-        {
-            for (int j=0; j<12; j++)
-            {
-                cout<<all_robot.Grid[i][j] <<" ";
-
-            }
-            cout<<endl;
-        }
-        cout<<"Total Turns: "<<all_robot.robotWithTurns[all_robot.robotSeq][0]<<endl;
+        generationCountRobot[generation].robotAvgFitness = cntTurn/totalRobot;
+        cout<<"Average fitness of generation "<<generation<<": "<<generationCountRobot[generation].robotAvgFitness<<endl;
+    }
+    for(int i=0;i<totalGeneration;i++)
+    {
+        //generationCountRobot[generation].robotAvgFitness = cntTurn/totalRobot;
+        cout<<"Average fitness of generation "<<i<<": "<<generationCountRobot[i].robotAvgFitness<<endl;
     }
     return 0;
 }
@@ -96,31 +132,32 @@ void allRobot::runRobot()
 {
     //cout<<"HI Antor "<<Grid[1][2]<<endl;
     int x=initX, y=initY, turns = 0;
-    cout<<"initial X: "<<x<<"Initial Y: "<<y<<endl;
+    cout<<"initial X: "<<x<<", Initial Y: "<<y<<endl;
+    //cout<<"initial energy: "<<robotWithEnergy[robotSeq][0]<<endl;
     //added a flag for matching a gene
     bool flag = false;
     while(1)
     {
-        if(robotWithEnergy[robotSeq][0]<0)
+        if(robotWithEnergy<=0)
         {
-            robotWithTurns[robotSeq][0] = turns;
-            cout<<"Turns: "<<robotWithTurns[robotSeq][0]<<endl;
+            robotWithTurns = turns;
+            //cout<<"Turns: "<<robotWithTurns<<endl;
             return;
         }
         if(Grid[x][y] == 9)
         {
-            robotWithEnergy[robotSeq][0]+=5;
+            robotWithEnergy+=5;
             turns++;
         }
         else if(Grid[x][y] == 1)
         {
-            turns += robotWithEnergy[robotSeq][0];
-            robotWithEnergy[robotSeq][0] = 0;
-            break;
+            turns += robotWithEnergy;
+            robotWithEnergy = 0;
+            continue;
         }
         else if(Grid[x][y] == 0)
         {
-            robotWithEnergy[robotSeq][0]--;
+            robotWithEnergy--;
             turns++;
         }
         int direction;
@@ -160,6 +197,36 @@ void allRobot::runRobot()
     }
 }
 
+void allRobot::sortRoboticGene(allRobot obj[])
+{
+    for(int i=0; i<5-1; i++)
+    {
+        for(int j=0; j<5-1-i; j++)
+        {
+            if(obj[j].robotWithTurns < obj[j+1].robotWithTurns)
+            {
+                cout<<obj[j].robotWithTurns<<" and "<<obj[j+1].robotWithTurns<<endl;
+                swap(obj[j].Gene,obj[j+1].Gene);
+                swap(obj[j].robotWithTurns,obj[j+1].robotWithTurns);
+            }
+        }
+    }
+}
+
+void allRobot::Reproduction(allRobot obj[])
+{
+    for(int i=0; i<4; i+=2)
+    {
+
+        for(int j=8; j<16; j++)
+        {
+            //cout<<obj[j].robotWithTurns<<" and "<<obj[j+1].robotWithTurns<<endl;
+            swap(obj[i].Gene[j],obj[i+1].Gene[j]);
+            //swap(obj[j].robotWithTurns,obj[j+1].robotWithTurns);
+        }
+    }
+}
+
 void allRobot::gridCreate()
 {
     for(int i=0; i<12; i++)
@@ -169,6 +236,10 @@ void allRobot::gridCreate()
             if(i==0 || i==11 || j==0 || j==11)
             {
                 Grid[i][j]=1;
+            }
+            else
+            {
+                Grid[i][j]=0;
             }
         }
         if(i!=0 && i!=11)
@@ -194,6 +265,7 @@ void allRobot::gridCreate()
 //Here Gene [][0] = North, Gene [][1] = South, Gene [][2] = East, Gene [][3] = West, Gene [][4] = Direction
 void allRobot::geneCreate()
 {
+
     Gene[0][0] = 0;
     Gene[0][1] = 0;
     Gene[0][2] = 0;
